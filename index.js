c8061e1b6130d51e99594d7e10d6cd8222077530
Arnaud Piroelle <piroelle.arnaud@gmail.com>
2019-04-04 20:38:27 +0200

    Enable manga update scheduler

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const PORT = process.env.PORT || 5000
 const server = new RestServer(PORT)
 const updateScheduler = new UpdateScheduler()
 
-//updateScheduler.registerMangasSync()
+updateScheduler.registerMangasSync()
 server.start()
