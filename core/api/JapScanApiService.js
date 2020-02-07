824a86290acb2d8665fbe49ae9adc746e4641199
Arnaud Piroelle <piroelle.arnaud@gmail.com>
2020-02-08 00:07:13 +0100

    Change headers

--- core/api/JapScanApiService.js
+++ core/api/JapScanApiService.js
@@ -1,10 +1,10 @@
 const headers = {
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.8",
-    "Accept-Encoding": "gzip, deflate, , br"
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br"
 }
 const cloudscraper = require('cloudscraper')
 const cheerio = require('cheerio')
