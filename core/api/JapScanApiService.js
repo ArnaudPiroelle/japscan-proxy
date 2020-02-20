8d090e6dbeee35e03df09825c721d9708378421d
Arnaud Piroelle <piroelle.arnaud@gmail.com>
2020-02-20 23:02:07 +0100

    Upgrade cloudscraper and fix headers

--- core/api/JapScanApiService.js
+++ core/api/JapScanApiService.js
@@ -1,12 +1,12 @@
 const headers = {
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
     "Accept-Encoding": "gzip, deflate, br"
 }
-const cloudscraper = require('cloudscraper')
+const cloudscraper = require('cloudscraper').defaults({ headers: headers })
 const cheerio = require('cheerio')
 const zlib = require('zlib')
 const crypto = require('crypto')
